7413e1eaf6ea06c0ead9762c2dede380f9d52e47
Muhammed Samed Çağlı <136382731+msamedcagli@users.noreply.github.com>
2025-05-08 09:28:06 +0300

    Add files via upload

---
+++ okulhtml/JSbuton/script.js
@@ -0,0 +1,8 @@
+function changeText() {
+    document.getElementById("text").textContent = "Yazı Değiştirildi!";
+  }
+
+  function getInfo() {
+    document.getElementById("info").textContent = "Telefon numarası:1234567890 \n Adres: Örnek Mah. No: 123 İstanbul \n E-posta:info@gmail.com";
+  }
+  
